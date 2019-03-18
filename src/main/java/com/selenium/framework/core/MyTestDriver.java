d509ff1fb0ae9b2e53d8fb269e81fa237f3255b9
peopleframe <peoleframe225@gmail.com>
2019-03-18 12:27:56 +0530

    Test

---
+++ src/main/java/com/selenium/framework/core/MyTestDriver.java
@@ -0,0 +1,21 @@
+package com.selenium.framework.core;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+
+public class MyTestDriver {
+	//Hello Test
+	
+	//New
+	
+	public static void main(String[] args) throws SecurityException,
+			IllegalArgumentException, NoSuchMethodException,
+			IllegalAccessException, InvocationTargetException, IOException {
+		Keywords kw = new Keywords();
+
+		TestDriver td = new TestDriver(kw, System.getProperty("user.dir")
+				+ "/config/config.properties");
+		td.start();
+	}
+}
